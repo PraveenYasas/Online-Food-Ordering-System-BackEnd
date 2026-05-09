32a57f27cddc93be9751985ee306c82fd2930f88
Praveen Yasas <praveenyasas2001@gmail.com>
2026-05-09 22:40:59 +0530

    Add initial Spring Boot FoodOrderingSystem project
    Scaffold a new Spring Boot project 'FoodOrderingSystem'. Adds pom.xml (Spring Boot 4.0.6, java.version=25) with dependencies for JPA, Security, WebMVC, MySQL connector and Lombok. Adds Maven Wrapper (mvnw, mvnw.cmd, .mvn/wrapper with wrapperVersion=3.3.4 and distributionUrl for Maven 3.9.15), .gitignore and .gitattributes, main application class, application.properties and a basic Spring Boot test. (Note: several .DS_Store files were included in the tree.)

---
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/FoodOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd113.FoodOrderingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ FoodOrderingSystem/src/test/java/lk/ijse/cmjd113/FoodOrderingSystem/FoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd113.FoodOrderingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
