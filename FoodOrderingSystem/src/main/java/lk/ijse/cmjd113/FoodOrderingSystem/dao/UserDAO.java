b2bdeaf3add1db1761604747d42afca4df0183df
Praveen Yasas <praveenyasas2001@gmail.com>
2026-05-26 23:17:33 +0530

    Add comments explaining UserDAO and JPA usage
    Add explanatory (Sinhala) comments to UserDAO.java describing the role of the repository layer, the @Repository annotation, and extending JpaRepository<UserEntity, Long]. Comments explain provided CRUD methods (save, findById, findAll, delete, deleteById) and Spring Data JPA derived query naming (e.g., findByEmail, existsByEmail). No functional code changes were made.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dao/UserDAO.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dao/UserDAO.java
@@ -1,5 +1,8 @@
 package lk.ijse.cmjd113.FoodOrderingSystem.dao;
 
+// Ape application ekai data bse eka athara katha baha karanne me layer eka haraha.
+// Data bse ekakth ekka kelinma sambanda wela data ganna, danna pawichchi karana kalla
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,10 +10,18 @@ import org.springframework.stereotype.Repository;
 
 import lk.ijse.cmjd113.FoodOrderingSystem.entities.UserEntity;
 
-// Ape application ekai data bse eka athara katha baha karanne me layer eka haraha.
-// Data bse ekakth ekka kelinma sambanda wela data ganna, danna pawichchi karana kalla
-
+// @Repository       - meken springboot ekata kiyanawa meka thama database ekath ekka ganu denu karana pradhana kalla kiyala.
 @Repository
+
+// extends JpaRepository<UserEntity, Long>.    - UserEntity kiyanne ape entitiy eka
+//                                             - Long kiyanne eke primary key eke data type eka.
+// meka extend kalahama apita data base ekata data danna, makanna, update karanna amuthuwen SQL code liyanna one naha.
+// (save(), findById(), findAll(), delete(), deleteById() widihata thiyana method walin data base ekata ganu denu karanna puluwan.)
+
+// Spring Data JPA walathiyana supirima de thama apita ona widihata name gahala method haduwahama 
+// Ex: (findByEmail, existsByEmail) Ekata adala SQL query eka ibema hadagannawa. 
+// Eka amuthuwen liyanna one naha.
+
 public interface UserDAO extends JpaRepository<UserEntity, Long> {
 
     // Email eka pawichchi karala user kenek wa hoyaganna liyapu method ekak. 
