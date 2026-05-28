732cd765f3c42d6b7d4c3f67f99e3f05b07e1264
Praveen Yasas <praveenyasas2001@gmail.com>
2026-05-28 23:36:48 +0530

    Comment out saveUser; add DTO-based notes
    Commented out the saveUser(UserEntity) method and added extensive inline comments describing a shift to a DTO-based service layer. The notes explain benefits such as hiding database schema details, avoiding persistence of confirmPassword, performing validation/mapping in the service layer, and reducing frontend impact when renaming entity fields. This change is documentation-only and does not alter runtime behavior.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/service/UserService.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/service/UserService.java
@@ -6,13 +6,41 @@ import lk.ijse.cmjd113.FoodOrderingSystem.entities.UserEntity;
 
 public interface UserService {
 
-    // 1. meka samanyayen karannne naha.
+    // // 1. meka samanyayen karannne naha.
 
-    //    meka kalahama wenne frontend eken ena data kelinma UserEntity ekata allagena eka kelinma database eke save wenawa.
+    // //    meka kalahama wenne frontend eken ena data kelinma UserEntity ekata allagena eka kelinma database eke save wenawa.
 
-    // meke thiyana awula thamai UserEntity eka hariyata ape data base eke kadapathak wage.
-    // eke database eke thiyana hama column ekakma thiyanawa.
-    // api eka kelinma eliyata expose kalahama, hacker kenekta hari pita kenekta hari ape database eke structure eka balaganna puluwan.
+    // // meke thiyana awula thamai UserEntity eka hariyata ape data base eke kadapathak wage.
+    // // eke database eke thiyana hama column ekakma thiyanawa.
+    // // api eka kelinma eliyata expose kalahama, hacker kenekta hari pita kenekta hari ape database eke structure eka balaganna puluwan.
 
-    UserEntity saveUser(UserEntity user);     // me method eka damme user ekak database eke save karanna.
+    // UserEntity saveUser(UserEntity user);     // me method eka damme user ekak database eke save karanna.
+
+    // 2. meka samanyayen kramayayen karana karamaya.
+
+    // Mekedi karanne samanyayen DTO pawichchi karala.
+    // aluthen UserDTO kiyala ekak haduwa. Eka nikan hariyata pearanayak athara madiyek wage nikan.
+    // Dan frontend eken kelinma data ewanne me DTO ekata.
+    // Itapasse service layer ekedi api e DTO eke thiyana data aragena alith userEntity ekak athulata dala (Map karala) Database ekata ywanawa.
+
+    // Me kramayen labena wasi 3k thiyanawa
+
+    // Uparima arakshawa 
+    // - Hithuwoth mehema issarahata api UserTable eke failedLoginAttempts (Waradiyata fields Login) gahapu wara ganana wage rahasigatha columna ekak thiyanawa kiyala.
+    // - Api DTO eka athulata e field eka noda idiyama, Frontend eken kauruhari hacker kenek eka aluthen ewwoth ape system eka eeka baraganne naha.
+    // - Database eke ewwa eliyata leak wenneth naha.
+    
+    // Amathara Data hasiraweema
+    // - Hithannako apita SignUp weddi password saha confiremPassword kiyala fields dekak thiyenawa kiyala.
+    // - Ape data bse eke confiremPassword kiyala ekak save karane naha. 
+    // - Habai aluth krameta apita UserDTO eka athulata confirmPassword eka dala Service Layer ekedi edeke samanada kiyala check karanawa.
+    // - Itapasse harinam witharak Password eka Etity ekakata dala Database eke save karanna puluwan.
+    // - Kalin kramayata meka karanna baha.
+
+    // Wenaskam walata Oroththu deema
+    // - Hithuwoth mehema, Issarahata column ekaka name ekak firstName indan f_name walata wenas karanna una kiyala.
+    // - parana kramayatanam ape mulu frontend code ekama wenas karanna wenawa. 
+    // - Habai aluth kramayata frontend eke digatam DTO eke thiyana firstName ma pawichchi karanawa.
+    // - "entity.setF_name(dto.getFirstName())" kiyala code eka poddak wenas karanna witharai thiyenne.
+    // - Anith kisi dekata haniyak wenne naha.
 }
