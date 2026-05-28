41e0045ddda17cc62446ea6c7c445bfc3cf26814
Praveen Yasas <praveenyasas2001@gmail.com>
2026-05-28 15:47:40 +0530

    Add empty UserDTO class
    Add a new UserDTO placeholder in lk.ijse.cmjd113.FoodOrderingSystem.dto to capture frontend user data. Comments explain the intended flow: controllers pass DTOs to services, services convert DTOs to entities before DAO usage to avoid exposing DB models. Class is currently empty and serves as a scaffold for future fields.

---
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package lk.ijse.cmjd113.FoodOrderingSystem.dto;
+
+// Controller kene service ekata eddi enna ona DTO ekak.
+// Itapasse Service eka athuledi thamai DTO eka Entity ekakata convert karala DAO ekata yawanne.
+// Ethakota Data base eke hadaya koiwelawaka wath eliyata expose wenne naa.
+
+// me class eken karanne frontend eken ena data tika catch karaganne.
+
+public class UserDTO {
+    
+}
