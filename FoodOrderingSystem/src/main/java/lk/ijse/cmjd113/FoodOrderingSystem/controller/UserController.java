3242f68478db7d04ad701a496565c970587c9577
Praveen Yasas <praveenyasas2001@gmail.com>
2026-06-01 23:26:46 +0530

    Annotate UserController as REST endpoint
    Convert UserController into a Spring REST controller by adding @RestController, @RequestMapping("/api/v1/users") and @CrossOrigin annotations and corresponding imports. This exposes the controller at /api/v1/users and enables CORS for frontend integration; also includes inline comments documenting the behavior.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/controller/UserController.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/controller/UserController.java
@@ -1,9 +1,17 @@
 package lk.ijse.cmjd113.FoodOrderingSystem.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 // me controller class eka hariyata ape receptionist kenek wage.
 // frontend eken ewana JSON data tika mulinma catch karaganne meya.
 // Allagena eka kelinma ara api hadapu UserService ekata pass karanawa.
 // "Menna meka save karala denna kiyala".
+
+@RestController                     // Meka API endpont ekak kiyyala Spring boot ekata kiyanne meken.
+@RequestMapping("/api/v1/users")    // Me Controller ekata enna ona prdhana URL eka.
+@CrossOrigin                        // front end eke port ekekin katha karanakota Block wenne nathiwenna meka danawa.
 
 public class UserController {
     
