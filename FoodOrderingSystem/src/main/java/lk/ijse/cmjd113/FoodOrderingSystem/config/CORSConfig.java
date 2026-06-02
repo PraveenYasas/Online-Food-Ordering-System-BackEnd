f0df0505dee5ad4f546c95272144bf5668e443e3
Praveen Yasas <praveenyasas2001@gmail.com>
2026-06-02 23:22:08 +0530

    Add ModelMapper bean to CORSConfig
    Annotate CORSConfig with @Configuration and register a ModelMapper @Bean so a single ModelMapper instance is available application-wide. Added necessary imports and brief explanatory comments.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/config/CORSConfig.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/config/CORSConfig.java
@@ -1,5 +1,14 @@
 package lk.ijse.cmjd113.FoodOrderingSystem.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration                  // meka configuration file eka kiyala Spring boot ekata kiyanwa.
 public class CORSConfig {
     
+    @Bean                       // meken karanne ModelMapper object ekak mulu app ekatama poduwe pawichchi karanna hadala den ekai.
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }  
 }
