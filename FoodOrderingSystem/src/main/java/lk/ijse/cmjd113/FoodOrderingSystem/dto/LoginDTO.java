16bfc9f9b7a44a18827c8be7591cab6a6ff6aa07
Praveen Yasas <praveenyasas2001@gmail.com>
2026-06-03 22:58:42 +0530

    Add Lombok and fields to LoginDTO
    Introduce Lombok annotations (@Data, @NoArgsConstructor, @AllArgsConstructor) to reduce boilerplate and add two fields (Email, Password) to LoginDTO. Add explanatory comments indicating this DTO captures only email and password from the React login flow.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dto/LoginDTO.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dto/LoginDTO.java
@@ -1,8 +1,19 @@
 package lk.ijse.cmjd113.FoodOrderingSystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 // React eken logweddi ewanne Email and Password witharaii.
 // Ee ena password and email catcj karaganna thama me class eka crete karanne.
+// Log wenakota apita ona wennenathi tika name, phone number wage ewwa catch karanna ona nathi nisa me class eka hadanne email and password witharaii.
+// anna ee tika arakshithawa catch karaganna thama mee DTO eka crete karaganne.
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class LoginDTO {
-    
+    private String Email;
+    private String Password;
 }
