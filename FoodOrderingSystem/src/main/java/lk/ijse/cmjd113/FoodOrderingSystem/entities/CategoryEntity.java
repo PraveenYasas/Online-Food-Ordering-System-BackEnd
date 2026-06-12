5b950ec6389de6f4512b73e07f87f1d6699e6004
Praveen Yasas <praveenyasas2001@gmail.com>
2026-06-12 23:22:19 +0530

    Add JPA annotations and fields to CategoryEntity
    Convert CategoryEntity into a JPA entity: added @Entity and @Table("categories"), primary key id with GenerationType.IDENTITY, and fields for name (nullable, unique) and description. Added Lombok annotations (@Data, @NoArgsConstructor, @AllArgsConstructor) to reduce boilerplate. Left a commented OneToMany mapping for foodItems to indicate intended relation.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/entities/CategoryEntity.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/entities/CategoryEntity.java
@@ -1,6 +1,34 @@
 package lk.ijse.cmjd113.FoodOrderingSystem.entities;
 
+// import java.util.List;
+
+// import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class CategoryEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    // @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    // private List<FoodItemEntity> foodItems;
 }
