56c2d52a21fac874ac01e149021eec323ce7cbb8
Praveen Yasas <149250835+PraveenYasas@users.noreply.github.com>
2026-06-14 23:00:45 +0530

    Add Sinhala comments to DAO interfaces
    Add Sinhala explanatory comments to CategoryDAO and FoodItemDAO to document the role of the JPA repositories (CRUD operations, no manual SQL needed) and the purpose of findByCategoryId. Also perform minor whitespace/formatting cleanup in FoodItemDAO. No functional changes.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dao/CategoryDAO.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dao/CategoryDAO.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import lk.ijse.cmjd113.FoodOrderingSystem.entities.CategoryEntity;
 
+// api kalin hadapu tables athare kathabaha karanna (Data danna, Ganna, Maknanna) palamak hadana eka.
+
 @Repository
 public interface CategoryDAO extends JpaRepository<CategoryEntity, Long> {
     // namen category ekak thiyanawada kiyala check karaganna onaunoth meka pawichch karanna puluwan.
--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dao/FoodItemDAO.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dao/FoodItemDAO.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Repository;
 
 import lk.ijse.cmjd113.FoodOrderingSystem.entities.FoodItemEntity;
 
+// api kalin hadapu tables athare kathabaha karanna (Data danna, Ganna, Maknanna) palamak hadana eka.
+
 @Repository
 public interface FoodItemDAO extends JpaRepository<FoodItemEntity, Long> { // me JPA repository ekeken extend kalahama api aaii amuthuwen SQL queris gahanna one naha.
-    
+
     // Issarahata apita "Burgers" Category walata adala kama witharak filter karala ganna ona unahama meka watinawa.
     List<FoodItemEntity> findByCategoryId(Long categoryId);
 }
