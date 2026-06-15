5160d0bc1ce3faf9af40fc988d6404ef8d5a3134
Praveen Yasas <149250835+PraveenYasas@users.noreply.github.com>
2026-06-15 22:49:02 +0530

    Add CategoryDTO with Lombok annotations
    Introduce CategoryDTO in lk.ijse.cmjd113.FoodOrderingSystem.dto as a simple data transfer object for categories. Uses Lombok @Data, @NoArgsConstructor, and @AllArgsConstructor and defines fields: Long id, String name, and String description.

---
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/dto/CategoryDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.cmjd113.FoodOrderingSystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
