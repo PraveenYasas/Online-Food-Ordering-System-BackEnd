4b18489e48fcded0747873bf0e5abd70a14a75cc
Praveen Yasas <149250835+PraveenYasas@users.noreply.github.com>
2026-06-16 20:56:56 +0530

    Add save and list methods to CategoryService
    Introduce CategoryDTO and java.util.List imports and add two service methods: saveCategory(CategoryDTO) to persist a category and getAllCategories() to return a list of categories. This extends the CategoryService interface to support creating and retrieving categories.

--- FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/service/CategoryService.java
+++ FoodOrderingSystem/src/main/java/lk/ijse/cmjd113/FoodOrderingSystem/service/CategoryService.java
@@ -1,5 +1,12 @@
 package lk.ijse.cmjd113.FoodOrderingSystem.service;
 
+import java.util.List;
+
+import lk.ijse.cmjd113.FoodOrderingSystem.dto.CategoryDTO;
+
 public interface CategoryService {
     
+    CategoryDTO saveCategory(CategoryDTO categoryDTO);
+
+    List<CategoryDTO> getAllCategories();
 }
